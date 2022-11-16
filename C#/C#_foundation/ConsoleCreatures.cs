67c46d19ab0ae9de78b0f135b6eda4300718164e
coding-james <mail.stroud@gmail.com>
2022-11-16 14:51:29 +0000

    Intro to C#

---
+++ C#/C#_foundation/ConsoleCreatures.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // These program will build an ASCII art Creature
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Ghosty");
+
+      // Dancing Man
+      //    "/" is an Escape Charcter, so use "//" to write a single "/"
+      Console.WriteLine("");
+      Console.WriteLine("c_     ___");
+      Console.WriteLine("　 \\\\__|__|__");
+      Console.WriteLine("　　 \\( o_o)");
+      Console.WriteLine("　　　 > ~  >");
+      Console.WriteLine("　　　/ 　 / \\\\");
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/");
+      Console.WriteLine("　　/　 /");
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\");
+      Console.WriteLine("　|　|  \\");
+      Console.WriteLine("　| / \\  )");
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+      Console.WriteLine("Dancing Man");
+     }
+  }
+}
---
+++ C#/C#_foundation/DataTypeErrors.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // int number = 38498.3222;
+      // changed type to double
+      double number = 38498.3222;
+
+      // dinosaur = "Barney";
+      // added type string
+      string dinosaur = "Barney";
+
+      // double lock = 293.000;
+      // lock is a reserved keyword
+      double lockCode = 293.000;
+
+      // bool is.yes = true;
+      // invalid name, can only use letter, numbers and underscore
+      bool isYes = true;
+
+      // string band = "The Low Anthem"
+      // added semi-colon ;
+      string band = "The Low Anthem";
+    }
+  }
+}
