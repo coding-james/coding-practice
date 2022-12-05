87628d3083622770b4743ac2e5c604828cd1a1c4
coding-james <mail.stroud@gmail.com>
2022-12-05 15:19:32 +0000

    JS Classes and Codewars JS Challenges

--- JavaScript/Codewars/unique_in_order.js
+++ JavaScript/Codewars/unique_in_order.js
@@ -1,32 +1,44 @@
-// CODEWARS.COM - Disemvowel Trolls
-var uniqueInOrder=function(iterable) {
-    if(iterable.constructor === String){
-        let unique = iterable.charAt(0);
-        for(let char=0; char < iterable.length; char++){
-            if(iterable.charAt(char) != unique.charAt(unique.length - 1)){
-                unique = unique + iterable.charAt(char);
+// CODEWARS.COM - Unique in order
+var uniqueInOrder = function (iterable) {
+    let unique = [];
+    if (iterable.length == 1 && iterable.includes('') && iterable.constructor === Array) {
+        // console.log("I'm an emptry string in an array");
+        unique = [''];
+        return unique;
+    } else if (iterable.length == 1 && iterable.includes(' ') && iterable.constructor === Array) {
+        // console.log("I'm an emptry string in an array");
+        unique = [''];
+        return unique;
+    } else if (iterable.constructor === String) {
+        unique = [iterable.charAt(0)];
+        for (let char = 0; char < iterable.length; char++) {
+            if (iterable.charAt(char) != unique[unique.length - 1]) {
+                unique.push(iterable.charAt(char));
             }
         }
-        console.log(unique);
-    } else if(iterable.constructor === Array){
-        // TODO: Finsih off this section & convert unique to arrays instead.
-        let unique = iterable[0];
-        // console.log("I'm an array");
-        console.log(iterable[0]);
-        for(let char=0; char < iterable.length; char++){
-            if(iterable[char] != unique.charAt(unique.length - 1)){
-                unique = unique + iterable[char];
+        return unique;
+    } else if (iterable.constructor === Array && iterable.length > 0) {
+        unique = [iterable[0]];
+        for (let char = 0; char < iterable.length; char++) {
+            if (iterable[char] != unique[unique.length - 1]) {
+                unique.push(iterable[char]);
             }
         }
+        return unique;
+    } else {
+        unique = [''];
+        return unique;
     }
-  }
-
+}
 
 console.log(uniqueInOrder('AAAABBBCCDAABBB'));
-// console.log("Expected: ['A', 'B', 'C', 'D', 'A', 'B']")
-  
 console.log(uniqueInOrder('ABBCcAD'));
-// console.log("Expected: ['A', 'B', 'C', 'c', 'A', 'D']")
-  
-console.log(uniqueInOrder([1,2,2,3,3]));
-// console.log("Expected: [1,2,3]")
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
+console.log(uniqueInOrder([]));
+console.log(uniqueInOrder([""]));
+console.log(uniqueInOrder(['']));
+console.log(uniqueInOrder(['A']));
+console.log(uniqueInOrder(['a']));
+console.log(uniqueInOrder([' ']));
+console.log(uniqueInOrder("A"));
+console.log(uniqueInOrder([ '' ]));
