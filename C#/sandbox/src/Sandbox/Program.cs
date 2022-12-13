4c64a08a286a94dbb11704a02a749bee72843823
coding-james <mail.stroud@gmail.com>
2022-12-13 17:56:56 +0000

    C# Sandbox, a simple calc

--- C#/sandbox/src/Sandbox/Program.cs
+++ C#/sandbox/src/Sandbox/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-if (args.Length > 0)
-{
-    Console.WriteLine($"Hello {args[0]}!"); //dotnet run -- NAME
-}
-else
-{
-    Console.WriteLine($"Hello!"); //will run if no name (argument) given
-}
+
+// Check for string arguments and customised greeting if true
+// if (args.Length > 0)
+// {
+//     Console.WriteLine($"Hello {args[0]}!"); //dotnet run -- NAME
+// }
+// else
+// {
+//     Console.WriteLine($"Hello!"); //will run if no name (argument) given
+// }
+
+// A simple calculation
+double x = 34.1;
+double y = 10.3;
+Console.WriteLine(x + y);
