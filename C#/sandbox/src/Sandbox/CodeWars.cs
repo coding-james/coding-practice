012aced04d3a3725d7f6eb98166ae7f97bd615bf
coding-james <mail.stroud@gmail.com>
2022-12-20 11:11:10 +0000

    C# codewars find the odd int

--- C#/sandbox/src/Sandbox/CodeWars.cs
+++ C#/sandbox/src/Sandbox/CodeWars.cs
@@ -68,5 +68,32 @@ namespace Sandbox
 
             return true;
         }
+
+        public static int find_it(int[] seq)
+        {
+            int? odd = null;
+            while (odd.HasValue == false)
+            {
+                foreach (int num in seq)
+                {
+                    int count = 0;
+                    for (int i = 0; i < seq.Length; i++ )
+                    {
+                        
+                        if (num == seq[i])
+                        {
+                            count++;
+                        }
+                    }
+                    
+                    if (count % 2 != 0)
+                    {
+                        odd = num;
+                    }
+
+                }                
+            }
+            return odd.Value;
+        }
     }
 }
