36071b2d0507af6aec6bc96c1e7b6f6a3965218f
coding-james <mail.stroud@gmail.com>
2022-12-20 09:56:41 +0000

    C# Codewars is prime

--- C#/sandbox/src/Sandbox/Program.cs
+++ C#/sandbox/src/Sandbox/Program.cs
@@ -16,4 +16,7 @@ Console.WriteLine("CODEWARS:");
 // Console.WriteLine(Sandbox.CodeWars.Disemvowel("This website is for losers LOL!"));
 // Console.WriteLine(Sandbox.CodeWars.Disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
 // Console.WriteLine(Sandbox.CodeWars.Disemvowel("What are you, a communist?"));
-Console.WriteLine(Sandbox.CodeWars.SquareDigits(9119));
+// Console.WriteLine(Sandbox.CodeWars.SquareDigits(9119));
+Console.WriteLine(Sandbox.CodeWars.IsPrime(0));
+Console.WriteLine(Sandbox.CodeWars.IsPrime(1));
+Console.WriteLine(Sandbox.CodeWars.IsPrime(2));
