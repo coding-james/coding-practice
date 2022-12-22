7d76912cb331afc33e8e24f943f1759051e787cc
James <98489957+coding-james@users.noreply.github.com>
2022-12-22 15:37:38 +0000

    C# Codewars Find the parity outlier

--- C#/sandbox/src/Sandbox/CodeWars.cs
+++ C#/sandbox/src/Sandbox/CodeWars.cs
@@ -144,5 +144,36 @@ namespace Sandbox
             }
             return resultArray;
         }
+
+        public static int Find(int[] integers)
+        {
+            int evenCount = 0;
+            int oddCount = 0;
+            int lastEven = int.MinValue;
+            int lastOdd = int.MinValue;
+            
+            foreach (int num in integers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenCount++;
+                    lastEven = num;
+                }
+                else
+                {
+                    oddCount++;
+                    lastOdd = num;
+                }
+            }
+
+            if (evenCount == 1)
+            {
+                return lastEven;
+            }
+            else
+            {
+                return lastOdd;
+            }
+        }
     }
 }
