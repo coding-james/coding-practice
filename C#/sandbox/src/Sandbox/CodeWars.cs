020de821e8a863eca31b6f5804cff950724b3b4c
James <98489957+coding-james@users.noreply.github.com>
2023-01-05 11:27:11 +0000

    C# Codewars Who Likes It

--- C#/sandbox/src/Sandbox/CodeWars.cs
+++ C#/sandbox/src/Sandbox/CodeWars.cs
@@ -212,5 +212,24 @@ namespace Sandbox
             while (result.ToString().Length > 1);
             return result;
         }
+
+        public static string Likes(string[] name)
+        {
+            switch (name.Length)
+            {
+                case 1:
+                    return $"{name[0]} likes this";
+                case 2:
+                    return $"{name[0]} and {name[1]} like this";
+                case 3:
+                    return $"{name[0]}, {name[1]} and {name[2]} like this";
+                case >= 4:
+                    int extra = name.Length - 2;
+                    return $"{name[0]}, {name[1]} and {extra} others like this";
+                default:
+                    return "no one likes this";
+            }
+
+        }
     }
 }
