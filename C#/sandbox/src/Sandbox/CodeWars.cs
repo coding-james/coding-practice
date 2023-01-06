594dbe8f9c37054c1015c828708a74965e89730e
James <98489957+coding-james@users.noreply.github.com>
2023-01-06 10:03:40 +0000

    Codewars Rock Paper Scissors

--- C#/sandbox/src/Sandbox/CodeWars.cs
+++ C#/sandbox/src/Sandbox/CodeWars.cs
@@ -236,5 +236,21 @@ namespace Sandbox
         {
             return $"({string.Join("", numbers[0..3])}) {string.Join("", numbers[3..6])}-{string.Join("", numbers[6..10])}";
         }
+
+        public static string Rps(string p1, string p2)
+        {
+            if (p1 == p2)
+            {
+                return "Draw!";
+            }
+            else if ((p1 == "rock" && p2 == "scissors") || (p1 == "paper" && p2 == "rock") || (p1 == "scissors" && p2 == "paper"))
+            {
+                return "Player 1 won!";
+            }
+            else
+            {
+                return "Player 2 won!";
+            }
+        }
     }
 }
