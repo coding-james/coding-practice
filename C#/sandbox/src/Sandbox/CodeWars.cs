0898a4b5ebb24cd60a2a0f9f68aca85e80e86275
James <98489957+coding-james@users.noreply.github.com>
2023-01-06 09:46:05 +0000

    C# Codewars Create Phone No

--- C#/sandbox/src/Sandbox/CodeWars.cs
+++ C#/sandbox/src/Sandbox/CodeWars.cs
@@ -231,5 +231,10 @@ namespace Sandbox
             }
 
         }
+
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return $"({string.Join("", numbers[0..3])}) {string.Join("", numbers[3..6])}-{string.Join("", numbers[6..10])}";
+        }
     }
 }
