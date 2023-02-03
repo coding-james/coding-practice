92391a275315aa6f879d237c327d76625a4eb450
James <98489957+coding-james@users.noreply.github.com>
2023-02-03 13:34:00 +0000

    CWars 5yu Moving Zeros To The End

--- C#/sandbox/src/Sandbox/Codewars/kyu5.cs
+++ C#/sandbox/src/Sandbox/Codewars/kyu5.cs
@@ -86,5 +86,34 @@ namespace CWars
             List<long> ipValue = new List<long>(Array.ConvertAll(ip.Split('.'), long.Parse));
             return (ipValue[0] * 256 * 256 * 256) + (ipValue[1] * 256 * 256) + (ipValue[2] * 256) + ipValue[3];
         }
+
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        // CODEWARS - Moving Zeros To The End
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            int i = 0;
+            int zeroCount = 0;
+            foreach (int a in arr)
+            {
+                if (a != 0)
+                {
+                    result[i] = a;
+                    i++;
+                }
+                else 
+                { 
+                    zeroCount++; 
+                }
+            }
+
+            for (int n = 1; n <= zeroCount; n++)
+            {
+                result[i] = 0;
+                i++;
+            }
+
+            return result;
+        }
     }
 }
