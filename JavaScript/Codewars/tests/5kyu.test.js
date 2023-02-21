0fa3e3ab86ccf7b1542c5f53fbdf604c55d4ae59
James <98489957+coding-james@users.noreply.github.com>
2023-02-21 14:12:06 +0000

    CWars 5kyu Maximum subarray sum

--- JavaScript/Codewars/tests/5kyu.test.js
+++ JavaScript/Codewars/tests/5kyu.test.js
@@ -1,6 +1,7 @@
 const {expect} = require("chai");
 const domainName = require("../5 kyu/domain_name");
 const persistence = require("../5 kyu/persistent_bugger");
+const maxSequence = require("../5 kyu/maximum_sum_subarray");
 
 // CODEWARS: Extract the domain name from a URL
 describe("Extract the domain name from a URL Tests", () => {
@@ -33,6 +34,24 @@ describe("Persistent Bugger", () => {
     tests.forEach(({arg, expected}) => {
         it(`should return ${expected}`, () => {
             expect(persistence(arg)).to.equal(expected);
+        });
+    });
+});
+
+// CODEWARS: Maximum subarray sum
+describe("Maximum subarray sum", () => {
+    const tests = [
+        {arg: [-2, 1, -3, 4, -1, 2, 1, -5, 4], expected: 6},
+        {arg: [-2, -3, -1, -5], expected: 0},
+        {arg: [], expected: 0},
+        {arg: [-1, 4, 3, 2, 1, -1], expected: 10},
+        {arg: [-1, 4, 3, 2, 1, -12, 12], expected: 12},
+        {arg: [-1, 4, 3, 2, 1, -1, 12], expected: 21},
+    ];
+
+    tests.forEach(({arg, expected}) => {
+        it(`should return ${expected}`, () => {
+            expect(maxSequence(arg)).to.equal(expected);
         });
     });
 });
