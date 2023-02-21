369bd3f73335951b1607e83628b845ca69e16da6
James <98489957+coding-james@users.noreply.github.com>
2023-02-21 10:21:52 +0000

    CodeWars JavaScript 6kyu Persistent Bugger

--- JavaScript/Codewars/tests/5kyu.test.js
+++ JavaScript/Codewars/tests/5kyu.test.js
@@ -1,5 +1,6 @@
 const {expect} = require("chai");
 const domainName = require("../5 kyu/domain_name");
+const persistence = require("../5 kyu/persistent_bugger");
 
 // CODEWARS: Extract the domain name from a URL
 describe("Extract the domain name from a URL Tests", () => {
@@ -16,6 +17,22 @@ describe("Extract the domain name from a URL Tests", () => {
     tests.forEach(({arg, expected}) => {
         it(`should return ${expected}`, () => {
             expect(domainName(arg)).to.equal(expected);
+        });
+    });
+});
+
+// CODEWARS: Persistent Bugger
+describe("Persistent Bugger", () => {
+    const tests = [
+        {arg: 39, expected: 3},
+        {arg: 4, expected: 0},
+        {arg: 25, expected: 2},
+        {arg: 999, expected: 4},
+    ];
+
+    tests.forEach(({arg, expected}) => {
+        it(`should return ${expected}`, () => {
+            expect(persistence(arg)).to.equal(expected);
         });
     });
 });
