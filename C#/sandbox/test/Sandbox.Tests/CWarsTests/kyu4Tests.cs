9c7718caaca51a0bbeb4336d10f7fcea20cef8bd
James <98489957+coding-james@users.noreply.github.com>
2023-04-09 11:45:49 +0100

    Codewars 4kyu Roman Numerals Helper

--- C#/sandbox/test/Sandbox.Tests/CWarsTests/kyu4Tests.cs
+++ C#/sandbox/test/Sandbox.Tests/CWarsTests/kyu4Tests.cs
@@ -193,5 +193,57 @@ namespace Sandbox.Tests
                   "on Sundays, made away with three-quarters of his income." })));
             }
         }
+
+        // CODEWARS: Roman Numerals Helper
+        public class RomanNumerals
+        {
+            [Theory]
+            [InlineData(1, "I")]
+            [InlineData(2, "II")]
+            [InlineData(4, "IV")]
+            [InlineData(5, "V")]
+            [InlineData(10, "X")]
+            [InlineData(21, "XXI")]
+            [InlineData(25, "XXV")]
+            [InlineData(50, "L")]
+            [InlineData(100, "C")]
+            [InlineData(500, "D")]
+            [InlineData(1000, "M")]
+            [InlineData(2000, "MM")]
+            [InlineData(3000, "MMM")]
+            [InlineData(1954, "MCMLIV")]
+            [InlineData(1990, "MCMXC")]
+            [InlineData(2008, "MMVIII")]
+            [InlineData(2014, "MMXIV")]
+
+            public void RomanNumerals_To_Roman(int input, string expected)
+            {
+                Assert.Equal(expected, Codewars.kyu4.romanNumeralHelper.ToRoman(input));
+            }
+
+            [Theory]
+            [InlineData(1, "I")]
+            [InlineData(2, "II")]
+            [InlineData(4, "IV")]
+            [InlineData(5, "V")]
+            [InlineData(10, "X")]
+            [InlineData(21, "XXI")]
+            [InlineData(25, "XXV")]
+            [InlineData(50, "L")]
+            [InlineData(100, "C")]
+            [InlineData(500, "D")]
+            [InlineData(1000, "M")]
+            [InlineData(2000, "MM")]
+            [InlineData(3000, "MMM")]
+            [InlineData(1954, "MCMLIV")]
+            [InlineData(1990, "MCMXC")]
+            [InlineData(2008, "MMVIII")]
+            [InlineData(2014, "MMXIV")]
+
+            public void RomanNumerals_From_Roman(int expected, string input)
+            {
+                Assert.Equal(expected, Codewars.kyu4.romanNumeralHelper.FromRoman(input));
+            }
+        }
     }
 }
