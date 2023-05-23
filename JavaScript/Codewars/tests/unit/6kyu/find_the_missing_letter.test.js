f1ce16ee668bbca5b1300894390a01f62d2f8af5
James <98489957+coding-james@users.noreply.github.com>
2023-05-23 13:03:10 +0100

    Codewars 6kyu Find the missing letter

---
+++ JavaScript/Codewars/tests/unit/6kyu/find_the_missing_letter.test.js
@@ -0,0 +1,20 @@
+const { assert } = require("chai");
+const findMissingLetter = require("../../../6 kyu/find_the_missing_letter");
+
+// CODEWARS: Find the missing letter
+describe("Find the missing letter", () => {
+    const tests = [
+        { arg: ['a', 'b', 'c', 'd', 'f'], expected: 'e' },
+        { arg: ['O', 'Q', 'R', 'S'], expected: 'P' },
+        { arg: ['a', 'c'], expected: 'b' },
+        { arg: ['A', 'C'], expected: 'B' },
+        { arg: ['a', 'b', 'd', 'e', 'f'], expected: 'c' },
+
+    ];
+
+    tests.forEach(({ arg, expected }) => {
+        it(`should return ${expected}`, () => {
+            assert.deepEqual(findMissingLetter(arg), expected);
+        });
+    });
+});
