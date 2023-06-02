8fe6a8982133fa1f37a586ef099857a536e8c9f5
James <98489957+coding-james@users.noreply.github.com>
2023-06-02 20:52:11 +0100

    freeCodeCamp US telephone number validator

---
+++ JavaScript/freeCodeCamp/telephone-number-validator.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* 
+    Valid Formats:
+    "555-555-5555",
+    "(555)555-5555",
+    "(555) 555-5555",
+    "555 555 5555",
+    "5555555555",
+    "1 555 555 5555"] 
+    */
+
+function telephoneCheck(str) {
+    return /^(1\s?)?\d{3}([-\s]?)\d{3}\2\d{4}$/.test(str) || /^(1\s?)?\(\d{3}\)\s?\d{3}[-\s]?\d{4}$/.test(str) ? true : false;
+}
+
+module.exports = { telephoneCheck };
